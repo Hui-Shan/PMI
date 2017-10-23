c2d8d867cffabda0b28ac92c8eb88af712212eff
Hui Shan <huishan_chan@hotmail.com>
2017-10-23 14:26:23 +0200

    Added ImageFilter base class and ThresholdImageFilter.h

---
+++ Code/Assignments/IPLib/ImageClasses/ImageFilter.h
@@ -0,0 +1,52 @@
+#include "../std_lib_facilities.h"
+
+// Base class for all image filters to be implemented.
+class ImageFilter
+{
+	typedef short T; // all we do is short
+public:
+	// Constructor.
+	ImageFilter() {};
+
+	// Destructor. This class does not own any free store allocated objects; 
+	// but derived classes might, so it's virtual and empty
+	virtual ~ImageFilter() {};
+
+	// Because derived classes can add data/function/type members, we want to
+	// avoid copying (and thus slicing)
+	ImageFilter(const ImageFilter&) = delete;
+	ImageFilter& operator=(const ImageFilter&) = delete;
+
+	// Set the input image for this filter
+	virtual void setInput(const vector<T>& i) {
+		// The input should be const. However, we cannot store it that way in our
+		// base class. Therefor we cast away the const and store a pointer to the
+		// original data. Upon using the input data in update(), we cast it back.
+		_input = const_cast<vector<T>* >(&i);
+	}
+
+	// Get the output image result of this filter; 
+	// available after calling update()
+	virtual vector<T> getOutput() const { return _output; };
+
+	// Update the image filter and compute the output
+	virtual void update()
+	{
+		// Didn't touch _input, cast it back to const; then execute()
+		execute(const_cast<const vector<T>&>(*_input));
+	}
+
+protected:
+	// Container for the output image
+	vector<T> _output;
+
+	// This method should be overloaded in your derived class and implement the
+	// image filter that fills _output
+	virtual void execute(const vector<T>& i) = 0;
+
+private:
+	// Temporary storage of a pointer to the image data. Because we do some
+	// const-magic, we keep it private.
+	vector<T>* _input;
+};
+
--- Code/Assignments/IPLib/ImageClasses/main.cpp
+++ Code/Assignments/IPLib/ImageClasses/main.cpp
@@ -1,4 +1,5 @@
 #include "ImageIOFactory.h"
+#include "ThresholdImageFilter.h"
 
 int main()
 {
@@ -15,18 +16,29 @@ int main()
 		auto image = io->read();
 		cout << "Read in " << filename << "\n";
 
-		outfilename = "..//..//data//brain_out_lib2.pip";
+		ThresholdImageFilter f;
+		f.setInput(image);
+		f.setThreshold(60);
+		f.update();
+		image = f.getOutput();
+
+		outfilename = "..//..//data//brain_out_thresholded.pip";
 		io = ImageIOFactory::getIO(outfilename);
 		io->write(image, { 109, 91, 80, 1, 1 }); // image, dimensions
 		delete io; io = nullptr;
 		cout << "Written image to " << outfilename << "\n";
-
+		
 		filename = mhdfile;
 		ImageIOBase* io2 = ImageIOFactory::getIO(filename);
 		auto image2 = io2->read();
 		cout << "Read in " << filename << "\n";
+		
+		f.setInput(image2);
+		f.setThreshold(60);
+		f.update();
+		image2 = f.getOutput();
 
-		outfilename = "..//..//data//brain_out_lib2.mhd";
+		outfilename = "..//..//data//brain_out_thresholded.mhd";
 		io2 = ImageIOFactory::getIO(outfilename);
 		io2->write(image2, { 109, 91, 80, 1, 1 }); // image, dimensions
 		delete io2; io2 = nullptr;
