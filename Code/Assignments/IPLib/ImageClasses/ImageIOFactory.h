88a998eac45ec3a99825cfa249257f158b31d2c4
Hui Shan <huishan_chan@hotmail.com>
2017-10-27 17:07:44 +0200

    added <memory> in includes, need to fix unique_ptr stuff

--- Code/Assignments/IPLib/ImageClasses/ImageIOFactory.h
+++ Code/Assignments/IPLib/ImageClasses/ImageIOFactory.h
@@ -4,34 +4,37 @@
 #include "ImageIOBase.h"
 #include "PipIO.h"
 #include "MhdIO.h"
+#include <memory>
 
 namespace hmc {
 	class ImageIOFactory
 	{
 	public:
-		static ImageIOBase* getIO(const string& filename);
+		static unique_ptr<ImageIOBase> getIO(const string& filename);
 	};
 
-	ImageIOBase* ImageIOFactory::getIO(const string& filename)
+	//template<ImageIOBase*>
+	unique_ptr<ImageIOBase> ImageIOFactory::getIO(const string& filename)
 		// Function returns pointer to image object of .pip or .mhd file
 	{
 		// find extension of filename
 		int index = filename.find_last_of(".");
 		std::string ext = filename.substr(index);
-
+		unique_ptr<ImageIOBase> upt;
 		// create new PipIO or MhdIO images on heap accordingly
 		// and return pointer to image
 		if (ext == ".pip") {
 			PipIO *img_ptr = NULL;
-			img_ptr = new PipIO(filename);
-			return img_ptr;
+			img_ptr = new PipIO(filename);						
 		}
 		else {
 			MhdIO *img_ptr = NULL;
 			img_ptr = new MhdIO(filename);
-			return img_ptr;
+			
 		}
+		upt = make_unique{ img_ptr };
 	}
+	
 }
 
 #endif /* IMAGEIOFACTORY_H */
